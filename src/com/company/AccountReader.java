58af861e5941c8c241bbc4a61f148c7dc26fac89
mecenasy <mecenasy@gmail.com>
2018-11-16 21:43:58 +0100

    parse account

--- src/com/company/AccountReader.java
+++ src/com/company/AccountReader.java
@@ -8,44 +8,63 @@ import static java.nio.file.Files.readAllLines;
 public class AccountReader {
     private Path pathFile;
     private List<String> readFile;
-    private List<String[]> listAccounts = new ArrayList<>();
 
-    public AccountReader(Path pathFile){
+    public AccountReader(Path pathFile) {
         this.pathFile = pathFile;
+        ReadFile();
     }
 
-    public AccountReader ReadFile(){
+    public AccountReader ReadFile() {
         try {
             this.readFile = readAllLines(pathFile);
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("nie udalo sie wczytać pliku");
         }
         return this;
     }
 
-    public AccountReader IsolateAccounts (){
-        for (int i =0 ; i < this.readFile.size()/4; i++) {
-            this.listAccounts.add(
+    public List<String[]> IsolateAccounts() {
+        List<String[]> listAccounts = new ArrayList<>();
+
+        for (int i = 0; i < this.readFile.size() / 4; i++) {
+            listAccounts.add(
                     new String[]{
                             this.readFile.get(i * 4),
-                            this.readFile.get((i * 4)+1),
-                            this.readFile.get((i * 4)+2),
+                            this.readFile.get((i * 4) + 1),
+                            this.readFile.get((i * 4) + 2),
                     }
             );
         }
-        return this;
+        return listAccounts;
     }
 
-    public  List<String[]> GetAllAcounts(){
-        return  this.listAccounts;
-    }
+    public List<String> GetAllAccounts() {
 
-    public boolean CheckAccountSumControl (int indexAccount){
-        int sum = 0;
-        String acount = new Digit().convertStringToAccountNumber(this.listAccounts.get(indexAccount));
-        for (int i = 1; i <= acount.length(); i++){
-            sum += i * Integer.parseInt(acount.substring(i-1, i));
+        List<String[]> listAccounts = IsolateAccounts();
+        List<String> result = new ArrayList<>();
+
+        for(String[] account: listAccounts){
+            String transformedAccount = new Digit().convertStringToAccountNumber(account);
+            if (CheckErrorAccount(transformedAccount)){
+                result.add(transformedAccount + "ILL");
+            }else  if(!CheckAccountSumControl(transformedAccount)){
+                result.add(transformedAccount + " ERR");
+            }else {
+                result.add(transformedAccount);
+            }
         }
-        return sum /11 == 0;
+        return result;
+    }
+
+    public boolean CheckErrorAccount(String account) {
+        return account.contains("?");
+    }
+
+    public boolean CheckAccountSumControl(String acount) {
+        int sum = 0;
+        for (int i = 1; i <= acount.length(); i++) {
+            sum += i * Integer.parseInt(acount.substring(i - 1, i));
+        }
+        return sum % 11 == 0;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,16 @@
 package com.company;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
-import static java.nio.file.Files.readAllLines;
 
 public class Main {
 
     public static void main(String[] args) {
         Digit digit = new Digit();
-        Path file = Paths.get("C:\\bank.txt");
+        Path file = Paths.get("C:\\bank1.txt");
         AccountReader reader = new AccountReader(file);
-        String [] d = reader.ReadFile().IsolateAccounts().GetAllAcounts().get(0);
+        System.out.println(reader.GetAllAccounts());
 
-        System.out.println(digit.convertStringToAccountNumber(d));
-        System.out.println(reader.CheckAccountSumControl(2));
     }
 }
